092aee0f5696dde5739b170b4188a863057c4d28
Giovane Delfino Junior <81388094+giovanedelfinojunior@users.noreply.github.com>
2021-07-07 09:48:48 -0300

    Add files via upload

---
+++ xadrez-console/xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,42 @@
+﻿namespace tabuleiro {
+    abstract class Peca {
+
+        public Posicao posicao { get; set; }
+        public Cor cor { get; set; }
+        public int qteMovimentos { get; set; }
+        public Tabuleiro tab { get; set; }
+
+        public Peca(Tabuleiro tab, Cor cor) {
+            this.posicao = null;
+            this.cor = cor;
+            this.qteMovimentos = 0;
+            this.tab = tab;
+        }
+
+        public void incrementarQtdeMovimentos() {
+            qteMovimentos++;
+        }
+        public void decrementarQtdeMovimentos() {
+            qteMovimentos--;
+        }
+        public bool existeMovimentosPossiveis() {
+            bool[,] mat = movimentosPossiveis();
+            for(int i = 0; i < tab.linhas; i++) {
+                for(int j = 0; j<tab.colunas; j++) {
+                    if(mat[ i, j ]) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool movimentoPossivel(Posicao pos) {
+            return movimentosPossiveis()[ pos.linha, pos.coluna ];
+        }
+
+        public abstract bool[,] movimentosPossiveis();
+
+
+    }
+}
